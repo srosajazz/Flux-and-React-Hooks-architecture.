b6b4a7c59dd175eb7cb738093c5c87f7083079ef
Sergio Rosa <scarlosjazz@gmail.com>
2019-07-31 21:31:18 -0400

    Implementing Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,84 @@
 import React from 'react';
+import { MdAddShoppingCart} from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList} from './styles';
 
 export default function Home() {
   return (
-    <div />
+    <ProductList>
+      <li>
+      <img src="https://static.netshoes.com.br/produtos/tenis-nike-dart-12-msl-masculino/26/D12-2683-026/D12-2683-026_detalhe1.jpg?resize=280:280" alt="Nike Roshe One"
+      />
+      <strong>Nike Roshe One</strong>
+      <span>U$142,00</span>
+      <button type="button">
+        <div>
+          <MdAddShoppingCart size={16} color="#fff"/> 5
+        </div>
+        <span>Adding to Cart shop</span>
+      </button>
+      </li>
+      <li>
+      <img src="https://static.netshoes.com.br/produtos/tenis-nike-dart-12-msl-masculino/26/D12-2683-026/D12-2683-026_detalhe1.jpg?resize=280:280" alt="Nike Roshe One"
+      />
+      <strong>Nike Roshe One</strong>
+      <span>U$168,00</span>
+      <button type="button">
+        <div>
+          <MdAddShoppingCart size={16} color="#fff"/> 5
+        </div>
+        <span>Adding to Cart shop</span>
+      </button>
+      </li>
+      <li>
+      <img src="https://static.netshoes.com.br/produtos/tenis-nike-dart-12-msl-masculino/26/D12-2683-026/D12-2683-026_detalhe1.jpg?resize=280:280" alt="Nike Roshe One"
+      />
+      <strong>Nike Roshe One</strong>
+      <span>U$147,00</span>
+      <button type="button">
+        <div>
+          <MdAddShoppingCart size={16} color="#fff"/> 5
+        </div>
+        <span>Adding to Cart shop</span>
+      </button>
+      </li>
+      <li>
+      <img src="https://static.netshoes.com.br/produtos/tenis-nike-dart-12-msl-masculino/26/D12-2683-026/D12-2683-026_detalhe1.jpg?resize=280:280" alt="Nike Roshe One"
+      />
+      <strong>Nike Roshe One</strong>
+      <span>U$127,00</span>
+      <button type="button">
+        <div>
+          <MdAddShoppingCart size={16} color="#fff"/> 5
+        </div>
+        <span>Adding to Cart shop</span>
+      </button>
+      </li>
+      <li>
+      <img src="https://static.netshoes.com.br/produtos/tenis-nike-dart-12-msl-masculino/26/D12-2683-026/D12-2683-026_detalhe1.jpg?resize=280:280" alt="Nike Roshe One"
+      />
+      <strong>Nike Roshe One</strong>
+      <span>U$147,00</span>
+      <button type="button">
+        <div>
+          <MdAddShoppingCart size={16} color="#fff"/> 5
+        </div>
+        <span>Adding to Cart shop</span>
+      </button>
+      </li>
+      <li>
+      <img src="https://static.netshoes.com.br/produtos/tenis-nike-dart-12-msl-masculino/26/D12-2683-026/D12-2683-026_detalhe1.jpg?resize=280:280" alt="Nike Roshe One"
+      />
+      <strong>Nike Roshe One</strong>
+      <span>U$187,00</span>
+      <button type="button">
+        <div>
+          <MdAddShoppingCart size={16} color="#fff"/> 5
+        </div>
+        <span>Adding to Cart shop</span>
+      </button>
+      </li>
+    </ProductList>
+
   );
 }
