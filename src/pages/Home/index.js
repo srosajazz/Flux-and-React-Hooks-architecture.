39221d957e6076e3f18e16da70b19e36a71738a1
Sergio Rosa <scarlosjazz@gmail.com>
2019-08-01 20:22:28 -0400

    Searching products in the API

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,84 +1,46 @@
-import React from 'react';
-import { MdAddShoppingCart} from 'react-icons/md';
+import React, { Component } from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
+import { formatPrice } from '../../util/format';
+import api from '../../services/api';
+import { ProductList } from './styles';
 
-import { ProductList} from './styles';
+export default class Home extends Component {
+  state = {
+    products: [],
+  };
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-      <img src="https://static.netshoes.com.br/produtos/tenis-nike-dart-12-msl-masculino/26/D12-2683-026/D12-2683-026_detalhe1.jpg?resize=280:280" alt="Nike Roshe One"
-      />
-      <strong>Nike Roshe One</strong>
-      <span>U$142,00</span>
-      <button type="button">
-        <div>
-          <MdAddShoppingCart size={16} color="#fff"/> 5
-        </div>
-        <span>Adding to Cart shop</span>
-      </button>
-      </li>
-      <li>
-      <img src="https://static.netshoes.com.br/produtos/tenis-nike-dart-12-msl-masculino/26/D12-2683-026/D12-2683-026_detalhe1.jpg?resize=280:280" alt="Nike Roshe One"
-      />
-      <strong>Nike Roshe One</strong>
-      <span>U$168,00</span>
-      <button type="button">
-        <div>
-          <MdAddShoppingCart size={16} color="#fff"/> 5
-        </div>
-        <span>Adding to Cart shop</span>
-      </button>
-      </li>
-      <li>
-      <img src="https://static.netshoes.com.br/produtos/tenis-nike-dart-12-msl-masculino/26/D12-2683-026/D12-2683-026_detalhe1.jpg?resize=280:280" alt="Nike Roshe One"
-      />
-      <strong>Nike Roshe One</strong>
-      <span>U$147,00</span>
-      <button type="button">
-        <div>
-          <MdAddShoppingCart size={16} color="#fff"/> 5
-        </div>
-        <span>Adding to Cart shop</span>
-      </button>
-      </li>
-      <li>
-      <img src="https://static.netshoes.com.br/produtos/tenis-nike-dart-12-msl-masculino/26/D12-2683-026/D12-2683-026_detalhe1.jpg?resize=280:280" alt="Nike Roshe One"
-      />
-      <strong>Nike Roshe One</strong>
-      <span>U$127,00</span>
-      <button type="button">
-        <div>
-          <MdAddShoppingCart size={16} color="#fff"/> 5
-        </div>
-        <span>Adding to Cart shop</span>
-      </button>
-      </li>
-      <li>
-      <img src="https://static.netshoes.com.br/produtos/tenis-nike-dart-12-msl-masculino/26/D12-2683-026/D12-2683-026_detalhe1.jpg?resize=280:280" alt="Nike Roshe One"
-      />
-      <strong>Nike Roshe One</strong>
-      <span>U$147,00</span>
-      <button type="button">
-        <div>
-          <MdAddShoppingCart size={16} color="#fff"/> 5
-        </div>
-        <span>Adding to Cart shop</span>
-      </button>
-      </li>
-      <li>
-      <img src="https://static.netshoes.com.br/produtos/tenis-nike-dart-12-msl-masculino/26/D12-2683-026/D12-2683-026_detalhe1.jpg?resize=280:280" alt="Nike Roshe One"
-      />
-      <strong>Nike Roshe One</strong>
-      <span>U$187,00</span>
-      <button type="button">
-        <div>
-          <MdAddShoppingCart size={16} color="#fff"/> 5
-        </div>
-        <span>Adding to Cart shop</span>
-      </button>
-      </li>
-    </ProductList>
+  async componentDidMount() {
+    const response = await api.get('products');
 
-  );
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
+
+    this.setState({ products: data });
+  }
+
+  render() {
+    const { products } = this.state;
+
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
+
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#FFF" /> 3
+              </div>
+
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+   }
 }
